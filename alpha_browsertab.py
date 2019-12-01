a57ea043eeaf7d99e60432a7ec31c20b4ce6be18
LuisSanchez-Dev <luis.sanchez.dev@hotmail.com>
2019-11-30 19:20:34 -0500

    Added new class BrowserTab to replace function add_browser_tab

--- alpha_browsertab.py
+++
@@ -1,7 +0,0 @@
-class BrowserTab:
-  def __init__(self, icon_string, tab_title, url):
-    self.icon = icon_string
-    self.title = tab_title
-    self.url = url
-  def register(self): pass
-  def unload(self): pass
